3f7fb8dc004e5b8edb17090e5b79463149251eea
NatalyaBortsova <natanatka31@gmail.com>
2022-01-31 14:45:46 +0200

    make about, footer

--- app/js/main.js
+++ app/js/main.js
@@ -78,29 +78,51 @@ $(function () {
 
   let sortBtn = document.querySelector('.portfolio__filter').children;
   let sortItem = document.querySelector('.portfolio__list').children;
-  
-  for(let i = 0; i < sortBtn.length; i++){
-      sortBtn[i].addEventListener('click', function(){
-          for(let j = 0; j< sortBtn.length; j++){
-              sortBtn[j].classList.remove('active');
-          }
-  
-          this.classList.add('active');
-          
-  
-          let targetData = this.getAttribute('data-target');
-  
-          for(let k = 0; k < sortItem.length; k++){
-              sortItem[k].classList.remove('active');
-              sortItem[k].classList.add('delete');
-  
-              if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
-                  sortItem[k].classList.remove('delete');
-                  sortItem[k].classList.add('active');
-              }
-          }
-      });
-  } 
-  
+
+  for (let i = 0; i < sortBtn.length; i++) {
+    sortBtn[i].addEventListener('click', function () {
+      for (let j = 0; j < sortBtn.length; j++) {
+        sortBtn[j].classList.remove('active');
+      }
+
+      this.classList.add('active');
+
+
+      let targetData = this.getAttribute('data-target');
+
+      for (let k = 0; k < sortItem.length; k++) {
+        sortItem[k].classList.remove('active');
+        sortItem[k].classList.add('delete');
+
+        if (sortItem[k].getAttribute('data-item') == targetData || targetData == "all") {
+          sortItem[k].classList.remove('delete');
+          sortItem[k].classList.add('active');
+        }
+      }
+    });
+  }
+
+
+const hiddenItems = document.querySelectorAll('.blog__item');
+const blogbutton = document.querySelector('.blog__button');
+
+let currentImg = 3;
+
+blogbutton.addEventListener('click', function(){
+  for(let i = currentImg; i < currentImg + 3; i++){
+    if (hiddenItems[i]) {
+      hiddenItems[i].style.display = 'block';
+    }
+  }
+  currentImg += 3;
+  if(currentImg >= hiddenItems.length) {
+    event.target.style.display = 'none';
+  }
+})
+
+const socialLink = document.querySelectorAll('.footer__social-link');
+
+socialLink.forEach((socialLink) => socialLink.addEventListener('click', (e) => e.preventDefault()));
+
 
 });
